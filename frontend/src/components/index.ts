306354c64324111180029e4adba120621f7dfc4f
broda-spendy <ikenna.m1903739@st.futminna.edu.ng>
2026-08-10 07:43:31 +0100

    feat(frontend): add loading Spinner and responsive Deposit & Transfer forms (Closes #47, #56) (#63)

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,7 +1,10 @@
 /**
  * Component barrel export.
  * Import from here rather than individual files:
- *   import { Header, CopyButton } from '../components'
+ *   import { Header, CopyButton, Spinner, DepositForm, TransferForm } from '../components'
  */
 export { Header } from './Header'
 export { CopyButton } from './CopyButton'
+export { Spinner } from './Spinner'
+export { DepositForm } from './DepositForm'
+export { TransferForm } from './TransferForm'
